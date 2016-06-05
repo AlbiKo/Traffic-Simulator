bff88020d06758e1c9c4376a7a7d687ac4ffbec2
Davaek <danikorra@hotmail.it>
2016-06-05 19:35:52 +0200

    Aggiunto prova di documentazione

---
+++ doc/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['faccio_20la_20prova_2e',['Faccio la prova.',['../index.html',1,'']]]
+];
---
+++ doc/html/search/variables_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['renderwindow',['renderWindow',['../classSFMLWidget.html#a8edb9d0f03b10d78b1291807e9070fcc',1,'SFMLWidget']]]
+];
