2cbcc0354400729497b7ed5f5d324981464aa88b
Becco96 <albibeccari@live.it>
2016-06-06 21:03:35 +0200

    Aggiunto SFML

--- setup-ide.sh
+++ setup-ide.sh
@@ -4,9 +4,7 @@ echo " - Codeblocks"
 echo " - G++ 4.9"
 echo " - GTKmm 3.0"
 echo " - Glade"
-echo " "
-echo "I seguenti pacchetti saranno scaricati:"
-echo " - SFML 2.3.2"
+echo " - SFML"
 echo " "
 read -p "Vuoi continuare? (Y/N): " yn
 	case $yn in
@@ -15,9 +13,6 @@ read -p "Vuoi continuare? (Y/N): " yn
 	esac
 add-apt-repository ppa:ubuntu-toolchain-r/test -y
 apt-get update
-apt-get install codeblocks g++-4.9 libgtkmm-3.0-dev glade -y
-wget -O SFML-2.3.2.tar.gz http://www.sfml-dev.org/files/SFML-2.3.2-linux-gcc-64-bit.tar.gz 
-tar -zxvf SFML-2.3.2.tar.gz
-rm SFML-2.3.2.tar.gz
+apt-get install codeblocks g++-4.9 libgtkmm-3.0-dev glade -y libsfml-dev
 echo " "
 echo "Eseguito."
