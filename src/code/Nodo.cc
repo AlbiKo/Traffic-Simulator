16a32838969b522efd43deb2ba5b16e1710f96cf
Davaek <danikorra@hotmail.it>
2016-06-10 23:30:43 +0200

    Prima stesura della classe Nodo

--- src/code/Nodo.cc
+++ src/code/Nodo.cc
@@ -1,5 +1,28 @@
 #include "../header/Nodo.h"
+#include "../header/Debug.h"
+
 
 Nodo::Nodo()
 {
+    pos.x 		 = 0;
+    pos.y 		 = 0;
+    numAdiacenze = 0;
+
+    for (int i = 0; i < 4; i++)
+        adiacenze[i] = NULL;
+
+	D2(std::cout<<"Nodo in posizione" <<pos.x <<"," <<pos.y <<std::endl);
+}
+
+Nodo::Nodo(Vector2i startPos)
+{
+    Nodo();
+    pos = startPos;
+
+    D1(std::cout<<"Nodo in posizione" <<pos.x <<"," <<pos.y <<std::endl);
+}
+
+Nodo::setPos(int x, int y)
+{
+
 }
--- src/header/Nodo.h
+++ src/header/Nodo.h
@@ -1,6 +1,34 @@
 #include <SFML/Graphics.hpp>
 
+
+using namespace sf;
+
+
 class Nodo
 {
+
+private:
+    Vector2i pos;
+
+    Nodo * adiacenze[4];
+    int numAdiacenze;
+
+public:
     Nodo();
+    Nodo(Vector2i startPos);
+
+	void setPos(int x, int y);
+
+	Nodo * getAdiacenza(int i);
+	void setAdiacenza(int i, Nodo * nodo);
+
+	inline int getNumAdiacenze()
+	{
+		return numAdiacenze;
+	}
+
+	inline Vector2i getPos()
+	{
+		return pos;
+	}
 };
