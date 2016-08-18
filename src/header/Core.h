96beb3d8ed6a33de2061d4f0e4b541e24543a951
Becco96 <albibeccari@live.it>
2016-08-18 09:47:26 +0200

    Aggiunto prototipo e documentazione di updateMacchina

--- src/header/Core.h
+++ src/header/Core.h
@@ -5,3 +5,7 @@
 
 void CoreInit();
 void update(sf::RenderWindow &widget);
+
+/**Aggiorna la posizione della macchina\n
+*  Se la macchina si trova su una curva o su un incrocio, cambia la direzione della macchina*/
+void updateMacchina(Macchina &macchina);
