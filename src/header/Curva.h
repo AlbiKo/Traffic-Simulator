5d3e0a98f6a70052aba9212d7205d3d8d9cfeaa0
Becco96 <albibeccari@live.it>
2016-08-18 19:20:30 +0200

    Aggiunta documentazione parziale

--- src/header/Curva.h
+++ src/header/Curva.h
@@ -14,6 +14,10 @@ public:
 	Curva(int rowIndex, int columnIndex, TipoBlocco tipo);
 
 	void cambiaVerso(TipoBlocco verso);
+	RectangleShape shape_1, shape_2, shape_3, shape_4; //DEBUG
+
+	/**Restituisce la direzione che deve prendere la macchina in prossimità di una curva.
+	*  @param pos Posizione attuale della macchina
+	*/
 	Direzione getChangeDir(Vector2f pos);
-	RectangleShape shape_1, shape_2, shape_3, shape_4;
 };
