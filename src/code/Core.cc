45ff8112098c31e0d4874cefe85847e064b42487
Becco96 <albibeccari@live.it>
2016-08-18 23:47:08 +0200

    Storie di una serata ispirata

--- src/code/Core.cc
+++ src/code/Core.cc
@@ -3,15 +3,54 @@
 using namespace sf;
 
 static Mappa mappa;
+Macchina * m;
+bool no = false;
+
+Direzionatore dd;
+Direzione dir = dd.estraiDirezione();
 
 void CoreInit()
 {
 	mappa.generate();
+	Vector2i a = mappa.getRandomSource();
+	D1(PRINT("x: " << a.x << " - y: " << a.y << "\n"));
+	int x1, y1;
+	Direzione d = Direzione::ND;
+	if (a.x == 0 && a.y != 0) {
+		d = Direzione::DX;
+		x1 = 40;
+		y1 = 48;
+		D1(PRINT("DX"));
+	}
+	if (a.x == 18 && a.y != 0) {
+		d = Direzione::SX;
+		D1(PRINT("SX"));
+		x1 = 40;
+		y1 = 19;
+	}
+	if (a.x != 0 && a.y == 0) {
+		d = Direzione::GIU;
+		D1(PRINT("GIU"));
+		x1 = 19;
+		y1 = 40;
+	}
+	if (a.x != 0 && a.y == 10) {
+		d = Direzione::SU;
+		D1(PRINT("SU"));
+		x1 = 48;
+		y1 = 40;
+	}
+	a.x = a.x * 68 + x1;
+	a.y = a.y * 68 + y1;
+
+	
+	m = new Macchina(a);
+	m->changeDirection(d);
 }
 
-Macchina m1 = Macchina(Vector2i(34, 3 * 68 - 20));
+/*Macchina m1 = Macchina(Vector2i(34, 3 * 68 - 20));
 Macchina m2 = Macchina(Vector2i(0, 1 * 68 - 20));
-Macchina m3 = Macchina(Vector2i(0, 5 * 68 - 20));
+Macchina m3 = Macchina(Vector2i(0, 5 * 68 - 20));*/
 
 
 
@@ -25,10 +64,16 @@ void updateMacchina(Macchina &macchina)
 		Curva * c = dynamic_cast<Curva *>(b);
 		if (c != NULL)
 			macchina.changeDirection(c->getChangeDir(macchina.getShape().getPosition())); //delego alla il cambio direzione della macchina
-		Incrocio4 * i4 = dynamic_cast<Incrocio4 *>(b);
-		if (i4 != NULL) {
-			Direzionatore random = Direzionatore();
-			macchina.changeDirection(i4->getChangeDir(macchina.getShape().getPosition(), Direzione::SX)); //delego all'incrocio instradare correttamente la macchina
+		Incroci * i = dynamic_cast<Incroci *>(b);
+		if (i != NULL) {
+			no = true;
+			macchina.changeDirection(i->getChangeDir(macchina.getShape().getPosition(), dir)); //delego all'incrocio instradare correttamente la macchina
+		}
+		else {
+			no = false;
+			dd.ripristina();
+			dd.escludiDirezione(getDirOpposta(macchina.getDirection()));
+			dir = dd.estraiDirezione();
 		}
 		if (isEmptyBlock(b->getTipo()))
 			macchina.stop();
@@ -41,20 +86,22 @@ void update(RenderWindow &widget)
 {	
 	mappa.draw(widget);
 	if (Keyboard::isKeyPressed(Keyboard::R)) {
-		mappa.generate();
-		m1.setPosition(Vector2i(34, 3 * 68 - 20));
-		m1.changeDirection(Direzione::DX);
+		CoreInit();
+		//m1.setPosition(Vector2i(34, 3 * 68 - 20));
+		//m1.changeDirection(Direzione::DX);
 	}
-	
-	m2.setColor(Color::Magenta);
+	m->setColor(Color::Yellow);
+	/*m2.setColor(Color::Magenta);
 	m3.setColor(Color::Green);
 	m1.draw(widget);
 	m2.draw(widget);
-	m3.draw(widget);
+	m3.draw(widget);*/
+	m->draw(widget);
 
-	updateMacchina(m1);
+	/*updateMacchina(m1);
 	updateMacchina(m2);
-	updateMacchina(m3);
+	updateMacchina(m3);*/
+	updateMacchina(*m);
 }
 
 
